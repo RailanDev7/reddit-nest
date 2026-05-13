c49f9907ffe22599c5a5d0a37d7c534e614f1b89
Railan <railandejesusdossantos@gmail.com>
2026-05-13 13:58:46 -0300

    feat: dto profile

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,37 @@
-export class CreateProfileDto {}
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProfileDto {
+    
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(500)
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUrl()
+  @MaxLength(500)
+  photo_url?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsUrl()
+  @MaxLength(500)
+  banner_url?: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  username!: string;
+
+  @IsInt()
+  userId!: number;
+}
